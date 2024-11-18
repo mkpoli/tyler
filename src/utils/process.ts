ba52d8fd186722f1f035385d12ff1721fededab3
mkpoli <git@mkpo.li>
2024-11-19 08:08:10 +0900

    feat: display git stdout realtime

---
+++ src/utils/process.ts
@@ -0,0 +1,20 @@
+import { spawn } from "node:child_process";
+
+export async function execAndRedirect(command: string): Promise<void> {
+	return new Promise((resolve, reject) => {
+		let [cmd, ...args] = command.split(" ");
+		const child = spawn(cmd, args, {
+			stdio: "inherit",
+		});
+		// child.stdout?.pipe(process.stdout);
+		child.stdout?.on("data", (data) => {
+			console.log(data.toString());
+		});
+		child.on("error", (error) => {
+			reject(error);
+		});
+		child.on("exit", () => {
+			resolve();
+		});
+	});
+}
